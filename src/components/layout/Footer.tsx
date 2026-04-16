1f6d07c778af374718049e61b13ab3cb2dc504f3
Yatinxo <yetithakur007@gmail.com>
2026-04-16 06:31:42 +0530

    Linkedin

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
         <span className="text-[0.6875rem] tracking-[0.4em] font-medium uppercase font-['Inter'] text-[#E5E2E1]/60">©2026 YETI</span>
       </div>
       <div className="flex gap-8">
-        <a className="text-[0.6875rem] tracking-[0.4em] font-medium uppercase font-['Inter'] text-primary hover:text-white transition-all cursor-ne-resize" href="#">LINKEDIN</a>
+        <a className="text-[0.6875rem] tracking-[0.4em] font-medium uppercase font-['Inter'] text-primary hover:text-white transition-all cursor-ne-resize" href="https://www.linkedin.com/in/yatin-thakur-3b13b8179" target="_blank" rel="noopener noreferrer">LINKEDIN</a>
         <a className="text-[0.6875rem] tracking-[0.4em] font-medium uppercase font-['Inter'] text-[#E5E2E1]/60 hover:text-[#7B2C1F] transition-all cursor-ne-resize" href="#">ARCHIVE</a>
         <a className="text-[0.6875rem] tracking-[0.4em] font-medium uppercase font-['Inter'] text-[#E5E2E1]/60 hover:text-[#7B2C1F] transition-all cursor-ne-resize" href="mailto:contact@yatinthakur.com">EMAIL</a>
       </div>
